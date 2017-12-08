48a424c905dc2dc1de2b28bcc20e0abb695ceb44
damionbrown <dbrown0591>
2017-12-08 15:55:19 -0600

    separate INSERT statements

---
+++ albums_seeder.sql
@@ -0,0 +1,24 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982','66','Pop, Rock, R&B');
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('AC DC', 'Back in Black', '1980','50','Hard Rock');
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', '1973','45','Progressive Rock');
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('Meat Loaf', 'Bat out of Hell', '1977','43','Hard rock, Progressive rock');
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('Whitney Houston', 'The Bodyguard', '1992','42','R&B, Soul, Pop, Soundtrack');
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('Eagles', 'TTheir Greatest Hits (1971–1975)', '1976','42','Rock, Soft rock, Folk rock');
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('Bee Gees / Various artists', 'Saturday Night Fever', '1977','40','Disco');
+
+INSERT INTO albums (artist, album_name, release_date, sales, genre)
+VALUES ('Fleetwood Mac', 'Rumours', '1977','40','Soft rock');
